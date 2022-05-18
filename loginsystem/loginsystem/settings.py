e117dd4365ae1bf96b6feba0b53aea953896c33f
Aniket Mukherjee <aniketmukherjee1509@gmail.com>
2022-05-18 10:06:51 +0530

    UPDATED CODE

--- loginsystem/loginsystem/settings.py
+++ loginsystem/loginsystem/settings.py
@@ -27,7 +27,7 @@ EMAIL_PORT = EMAIL_PORT
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-os+$i9pt^cpl5pb(xa@y&!p06-i#uuqh)lndgejh2jasfebh*+'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
